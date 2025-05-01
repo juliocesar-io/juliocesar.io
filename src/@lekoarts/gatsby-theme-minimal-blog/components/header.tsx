4621462aba34b0ddaaa600ca5a7bdd323266161a
Julio César <jc@juliocesar.io>
2025-05-01 18:56:38 -0500

    added home link

--- src/@lekoarts/gatsby-theme-minimal-blog/components/header.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/header.tsx
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 import { jsx, Flex } from "theme-ui"
+import { Link } from "gatsby"
 import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config"
 import ColorModeToggle from "@lekoarts/gatsby-theme-minimal-blog/src/components/colormode-toggle"
 import Navigation from "@lekoarts/gatsby-theme-minimal-blog/src/components/navigation"
@@ -13,21 +14,23 @@ const Header = () => {
     <header sx={{ mb: [5, 6], marginBottom: `0px !important`}}>
       <Flex sx={{ alignItems: `center`, justifyContent: `space-between` }}>
         <HeaderTitle />
-        <Text children={`Julio César`} sx={{ 
-                fontSize: [3, 4, 4], 
-                fontWeight: `bold`, 
-                color: `heading`,
-                '&::before': {
-                  content: '"|"',
-                  animation: 'blink 1s step-end infinite',
-                  marginRight: '5px',
-                  fontSize: '1.2em'
-                },
-                '@keyframes blink': {
-                  '0%, 100%': { opacity: 1 },
-                  '50%': { opacity: 0 }
-                }
-            }} />
+        <Link to="/" sx={{ textDecoration: 'none' }}>
+          <Text children={`Julio César`} sx={{ 
+                  fontSize: [3, 4, 4], 
+                  fontWeight: `bold`, 
+                  color: `heading`,
+                  '&::before': {
+                    content: '"|"',
+                    animation: 'blink 1s step-end infinite',
+                    marginRight: '5px',
+                    fontSize: '1.2em'
+                  },
+                  '@keyframes blink': {
+                    '0%, 100%': { opacity: 1 },
+                    '50%': { opacity: 0 }
+                  }
+              }} />
+        </Link>
         <ColorModeToggle />
       </Flex>
       <br />
